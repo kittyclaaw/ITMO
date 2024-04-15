e7ac5cdff63cb2db9b280e473fee5d6e0d55119e
kittyclaw <145503549+kittyclaaw@users.noreply.github.com>
2024-04-15 03:32:44 +0300

    Creation_script.sql

--- Базы Данных/lab1/scheme.sql
+++ Базы Данных/lab1/scheme.sql
@@ -1,5 +1,4 @@
 BEGIN;
-
     CREATE TYPE DISEASE_PAIN_LEVEL AS ENUM(
     'очень сильно',
     'сильно',
@@ -11,8 +10,8 @@ CREATE TABLE IF NOT EXISTS human(
     id SERIAL NOT NULL PRIMARY KEY,
     human_name text NOT NULL,
     human_state text NOT NULL,
-    human_disease_status BOOLEAN NOT NULL,
-    human_disease_chance INT
+    human_disease_status BOOLEAN NOT NULL DEFAULT FALSE,
+    human_immunity BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE IF NOT EXISTS method(
@@ -26,14 +25,15 @@ CREATE TABLE IF NOT EXISTS method(
 CREATE TABLE IF NOT EXISTS conclusion(
     id SERIAL NOT NULL PRIMARY KEY,
     method_id INT NOT NULL REFERENCES method(id),
-    conclusion_time text NOT NULL,
+    conclusion_time TIME,
     conclusion_result BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE IF NOT EXISTS verification(
     id SERIAL NOT NULL PRIMARY KEY,
     conclusion_id INT NOT NULL REFERENCES conclusion(id),
-    verification_time text NOT NULL,
+    time_start TIME NOT NULL,
+    time_end TIME,
     verification_state BOOLEAN NOT NULL DEFAULT FALSE
 );
 
@@ -54,7 +54,10 @@ CREATE TABLE IF NOT EXISTS disease(
     item_id INT REFERENCES item(id),
     disease_name text NOT NULL,
     disease_state BOOLEAN NOT NULL DEFAULT FALSE,
-    disease_pain_level DISEASE_PAIN_LEVEL
+    disease_pain_level DISEASE_PAIN_LEVEL,
+    disease_syptoms text,
+    start_date DATE,
+    end_date DATE
 );
 
 CREATE TABLE IF NOT EXISTS question(
@@ -62,24 +65,23 @@ CREATE TABLE IF NOT EXISTS question(
     item_id INT REFERENCES item(id),
     question_text text NOT NULL
 );
+INSERT INTO human(human_name, human_state, human_disease_status, human_immunity)
+    VALUES ('смотрящий на Луну', 'размышляет', FALSE, FALSE);
 
 INSERT INTO method(human_id, method_target, method_type, method_difficulty)
     VALUES ('1', 'кругляши', 'абстрактного', 'первоклассную');
 
-INSERT INTO human(human_name, human_state, human_disease_status, human_disease_chance)
-    VALUES ('смотрящий на Луну', 'unk', FALSE, 10);
-
 INSERT INTO conclusion(method_id, conclusion_time, conclusion_result)
-    VALUES ('1', '3-4 минуты', TRUE);
+    VALUES ('1', '04:05-08:00', TRUE);
 
-INSERT INTO verification(conclusion_id, verification_state, verification_time)
-    VALUES ('1', TRUE, 'немедленно');
+INSERT INTO verification(conclusion_id, verification_state, time_start, time_end)
+    VALUES ('1', TRUE, '00:08:00', '00:10:00');
 
 INSERT INTO item(conclusion_id, item_name, item_color, item_form, item_density, item_size, item_taste)
     VALUES ('1', 'голыши', 'белые', 'круглые', 'мягкие', 'большой', 'очень вкусны');
 
-INSERT INTO disease(human_id, item_id, disease_name, disease_state, disease_pain_level)
-    VALUES ('1', '1', 'unk', FALSE);
+INSERT INTO disease(human_id, item_id, disease_name, disease_state, disease_pain_level, start_date, end_date)
+    VALUES ('1', '1', 'unk', FALSE, 'сильно', '2001-02-01', '2001-02-03');
 
 INSERT INTO question(item_id, question_text)
     VALUES ('1', 'Может быть, и этот, большой, тоже?..');
